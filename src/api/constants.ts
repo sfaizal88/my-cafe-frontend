016185dc8711602d8e6ce730d14039e63faa39b7
Ahamed Faizal <faizalahd4@gmail.com>
2024-09-02 12:36:25 +0800

    API ulr update

--- src/api/constants.ts
+++ src/api/constants.ts
@@ -4,15 +4,17 @@
  * @date - 23rd August 2024
  */
 // CAFE SHOP API
-export const GET_ALL_CAFE_SHOP_API = "http://localhost:3002/api/cafes/getAllCafeShop";
-export const GET_CAFE_SHOP_API = "http://localhost:3002/api/cafes/getCafeShopDetailsById";
-export const CREATE_SHOP_CAFE_API = "http://localhost:3002/api/cafes/createShopCafe";
-export const UPDATE_SHOP_CAFE_API = "http://localhost:3002/api/cafes/updateShopCafe";
-export const DELETE_SHOP_CAFE_API = "http://localhost:3002/api/cafes/deleteShopCafeById";
+// http://localhost:3002
+const HOST = "https://my-cafe-backend.onrender.com/";
+export const GET_ALL_CAFE_SHOP_API = `${HOST}api/cafes/getAllCafeShop`;
+export const GET_CAFE_SHOP_API = `${HOST}api/cafes/getCafeShopDetailsById`;
+export const CREATE_SHOP_CAFE_API = `${HOST}api/cafes/createShopCafe`;
+export const UPDATE_SHOP_CAFE_API = `${HOST}api/cafes/updateShopCafe`;
+export const DELETE_SHOP_CAFE_API = `${HOST}api/cafes/deleteShopCafeById`;
 
 // EMPLOYEE API
-export const GET_ALL_EMPLOYEE_API = "http://localhost:3002/api/employees/getAllEmployee";
-export const GET_EMPLOYEE_API = "http://localhost:3002/api/employees/getEmployeeDetailsById";
-export const CREATE_EMPLOYEE_API = "http://localhost:3002/api/employees/createEmployee";
-export const UPDATE_EMPLOYEE_API = "http://localhost:3002/api/employees/updateEmployee";
-export const DELETE_EMPLOYEE_API = "http://localhost:3002/api/employees/deleteEmployeeById";
+export const GET_ALL_EMPLOYEE_API = `${HOST}api/employees/getAllEmployee`;
+export const GET_EMPLOYEE_API = `${HOST}api/employees/getEmployeeDetailsById`;
+export const CREATE_EMPLOYEE_API = `${HOST}api/employees/createEmployee`;
+export const UPDATE_EMPLOYEE_API = `${HOST}api/employees/updateEmployee`;
+export const DELETE_EMPLOYEE_API = `${HOST}api/employees/deleteEmployeeById`;
